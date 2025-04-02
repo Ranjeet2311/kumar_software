3f725724278f672d6c29520f332e5b6c8df78dea
Ranjeet kumar <ranjeetkumar2311@gmail.com>
2025-04-02 20:41:47 +0200

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,46 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "./globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "@/components/navbar/Navbar";
 import { ReduxProvider } from "@/store/slices/provider";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import ChakraClientWrapper from "@/components/ChakraClientWrapper";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 export const metadata: Metadata = {
-  title: "Kumar Software",
+  title: "Kumar Software - Dinamične Spletne Strani & Spletne Aplikacije",
   description:
-    "We start by understanding your business needs, challenges, and objectives. Through interactive sessions, we discuss your vision for the project. The target audience and user expectations Specific features, functionality, and scope of work. During this phase, we gather all the critical information and establish a clear roadmap, ensuring we’re aligned on your goals from the very beginning. Our goal is to collaborate with you as a partner to turn your ideas into reality.",
+    "V Kumar Softwares razvijamo dinamične spletne strani in zmogljive spletne aplikacije z modernim dizajnom, brezskrbno funkcionalnostjo in izjemnimi zmogljivostmi. Preoblikujte svoje poslovanje z digitalnimi rešitvami, prilagojenimi vašim potrebam.",
+  keywords:
+    "Kumar Software, dinamične spletne strani, spletne aplikacije, HTML5, CSS3, SCSS, JavaScript, TypeScript, React, Vue, Angular, GitHub, GitLab, Bootstrap, Tailwind, Webpack, Vite, Jest, Cypress, Node.js, Express.js, PHP, MongoDB, SQL, Docker, Figma, Adobe XD, Jira, EsLint, WordPress, WooCommerce",
   icons: {
     icon: "/favicon.png",
     apple: "/favicon.png",
+  },
+  openGraph: {
+    title: "Kumar Software - Spletne Aplikacije & Dinamične Spletne Strani",
+    description:
+      "Kumar Softwares je specializiran za izdelavo dinamičnih spletnih strani in zmogljivih spletnih aplikacij z modernim dizajnom in izjemnimi zmogljivostmi.",
+    url: "https://kumar-software.netlify.app/",
+    siteName: "Kumar Software",
+    images: [
+      {
+        url: "/assets/images/code_frontend.png",
+        width: 1200, // Recommended width
+        height: 630, // Recommended height (aspect ratio 1.91:1)
+        alt: "Kumar Software Social Media Image", // Alt text for accessibility
+      },
+    ],
+    locale: "sl_SI", // Locale for Slovene
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image", // Large image card for Twitter
+    site: "@kumarssoftware", // Twitter handle of the company
+    creator: "@kumarssoftware", // Creator's Twitter handle
+    title: "Kumar Software - Spletne Aplikacije & Dinamične Spletne Strani",
+    description:
+      "Transformirajte svoje podjetje z modernimi spletnimi stranmi in spletnimi aplikacijami, ki jih razvije Kumar Softwares.",
+    images: ["/assets/images/code_frontend.png"],
   },
 };
 
@@ -34,7 +50,15 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="sl">
+      <head>
+        {/* Manually adding hreflang links */}
+        <link
+          rel="alternate"
+          href="https://kumar-software.netlify.app/"
+          hrefLang="sl"
+        />
+      </head>
       <body>
         <ReduxProvider>
           <ChakraClientWrapper>
